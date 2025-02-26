912b0a98f13426d7347dac666e85b478cc4f508d
Ивайла Маркова (Ivayla Markova) <154016464+ivimarkova@users.noreply.github.com>
2025-02-27 00:45:09 +0200

    Add files via upload

--- src/Pages/Board.jsx
+++ src/Pages/Board.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import '../Assets/Styles/Board.css'; 
+import carrier from '../Assets/Images/ships/carrier.png'; // Импорт на изображенията
+import cruiser from '../Assets/Images/ships/cruiser.png';
+import battleship from '../Assets/Images/ships/battleship.png';
+import destroyer from '../Assets/Images/ships/destroyer.png'
 
 const Board = ({username}) => {
   // 10x10 клетки
@@ -12,6 +16,13 @@ const Board = ({username}) => {
         {/*<h2>{username}, place your ships!</h2>  */}
         <h2> Place your ships!</h2>
         <div className="board">{cells}</div>
+        
+      <div className="ships-container">
+        <img src={carrier} alt="Carrier" className="ship" />
+        <img src={cruiser} alt="Cruiser" className="ship" />
+        <img src={battleship} alt="Battleship" className="ship" />
+        <img src={destroyer} alt="Destroyer" className="ship" />
+      </div>
     </div>
   );
 };
